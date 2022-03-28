c156f6782c5fc42ec1e43ec73c903682fb5e0a7d
Delakroa <katon1986@gmail.com>
2022-03-28 20:13:58 +0300

    try-exept

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,3 +1,5 @@
+import json
+import requests
 import telebot
 
 TOKEN = ""
@@ -9,6 +11,10 @@ keys = {
     'эфириум': 'ETH',
     'доллар': 'USD'
 }
+
+
+class ConvertionException(Exception):
+    pass
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -25,6 +31,38 @@ def values(message: telebot.types.Message):
     for key in keys.keys():
         text = '\n'.join((text, key,))
     bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    values = message.text.split(' ')
+
+    if len(values) > 3:
+        raise ConvertionException('Слишком много параметров.')
+
+    quote, base, amount = values
+
+    if quote == base:
+        raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
+
+    try:
+        quote_ticker = keys[quote]
+    except KeyError:
+        raise ConvertionException(f'Не удалось обработать валюту {quote}')
+    try:
+        base_ticker = keys[base]
+    except KeyError:
+        raise ConvertionException(f'Не удалось обработать валюту {base}')
+
+    try:
+        amount = float(amount)
+    except ValueError:
+        raise ConvertionException(f'Не удалось обработать количество {amount}')
+
+    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+    total_base = json.loads(r.content)[keys[base]]
+    text = f'Цена {amount} {quote} в {base} - {total_base}'
+    bot.send_message(message.chat.id, text)
 
 
 # @bot.message_handler()
