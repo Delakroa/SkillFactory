92ccfc810f3abb701b0a153d306148bf48f071c6
Delakroa <katon1986@gmail.com>
2022-03-29 19:09:56 +0300

    refactoring

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,24 +1,12 @@
-import json
-import requests
 import telebot
-
-TOKEN = ""
+from config import keys, TOKEN
+from utils import ConvertionException, CryptoConverter
 
 bot = telebot.TeleBot(TOKEN)
 
-keys = {
-    'биткоин': 'BTC',
-    'эфириум': 'ETH',
-    'доллар': 'USD'
-}
-
-
-class ConvertionException(Exception):
-    pass
-
 
 @bot.message_handler(commands=['start', 'help'])
-def help_message(message: telebot.types.Message):
+def help(message: telebot.types.Message):
     text = 'Чтобы начать работу введите команду боту в следующем формате: \n<имя валюты> ' \
            '<в какую перевести> ' \
            '<количество переводимой валюты> \nУвидеть список всех доступных валют: /values'
@@ -37,30 +25,12 @@ def values(message: telebot.types.Message):
 def convert(message: telebot.types.Message):
     values = message.text.split(' ')
 
-    if len(values) > 3:
+    if len(values) != 3:
         raise ConvertionException('Слишком много параметров.')
 
     quote, base, amount = values
+    total_base = CryptoConverter.convert(quote, base, amount)
 
-    if quote == base:
-        raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
-
-    try:
-        quote_ticker = keys[quote]
-    except KeyError:
-        raise ConvertionException(f'Не удалось обработать валюту {quote}')
-    try:
-        base_ticker = keys[base]
-    except KeyError:
-        raise ConvertionException(f'Не удалось обработать валюту {base}')
-
-    try:
-        amount = float(amount)
-    except ValueError:
-        raise ConvertionException(f'Не удалось обработать количество {amount}')
-
-    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-    total_base = json.loads(r.content)[keys[base]]
     text = f'Цена {amount} {quote} в {base} - {total_base}'
     bot.send_message(message.chat.id, text)
 
