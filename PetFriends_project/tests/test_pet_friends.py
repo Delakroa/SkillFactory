0d30ec2891710d0fa69f0bad909189db7dd91d4d
Delakroa <katon1986@gmail.com>
2022-04-09 19:17:11 +0300

    test_get_all_pets_with_valid_key

--- PetFriends_project/tests/test_pet_friends.py
+++ PetFriends_project/tests/test_pet_friends.py
@@ -7,4 +7,11 @@ pf = PetFriends()
 def test_get_api_key_for_valis_user(email=valid_email, password=valid_password):
     status, result = pf.get_api_key(email, password)
     assert status == 200
-    assert "key" in result  # result == "d524b1fb97990041ae6c5138c688ac35ab0292a971e458827b2f41fd"
+    assert "key" in result
+
+
+def test_get_all_pets_with_valid_key(filter=''):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.get_list_of_pets(auth_key, filter)
+    assert status == 200
+    assert len(result['pets']) > 0
