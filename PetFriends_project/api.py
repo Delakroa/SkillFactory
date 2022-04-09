c0c586b25557355b96d7c118f6be1c41d1574c63
Delakroa <katon1986@gmail.com>
2022-04-09 13:27:02 +0300

    api clacc PetsFriends

---
+++ PetFriends_project/api.py
@@ -0,0 +1,23 @@
+import requests
+
+
+class PetFriends:
+    def __init__(self):
+        """Инициализация сайта дом питомца"""
+        self.base_url = "https://petfriends1.herokuapp.com/"
+
+    def get_api_key(self, email, password):
+        """Получение аутентификационнго ключа"""
+        headers = {
+            'email': email,
+            'password': password
+        }
+
+        res = requests.get(self.base_url + 'api/key', headers=headers)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return result, status
