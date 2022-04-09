768f703890403251a34c4fe96690bbe319ae4c82
Delakroa <katon1986@gmail.com>
2022-04-09 14:06:16 +0300

    unit_test

--- PetFriends_project/tests/test_pet_friends.py
+++ PetFriends_project/tests/test_pet_friends.py
@@ -7,4 +7,4 @@ pf = PetFriends()
 def test_get_api_key_for_valis_user(email=valid_email, password=valid_password):
     status, result = pf.get_api_key(email, password)
     assert status == 200
-    assert "key" in result  # result == "ea738148a1f19838e1c5d1413877f3691a3731380e733e877b0ae729"
+    assert "key" in result  # result == "d524b1fb97990041ae6c5138c688ac35ab0292a971e458827b2f41fd"
