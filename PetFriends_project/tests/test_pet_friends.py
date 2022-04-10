b75a46f189fcdc6679941e59bf917c7bd1f13940
Delakroa <katon1986@gmail.com>
2022-04-10 14:13:15 +0300

    refactoring

--- PetFriends_project/tests/test_pet_friends.py
+++ PetFriends_project/tests/test_pet_friends.py
@@ -5,13 +5,24 @@ pf = PetFriends()
 
 
 def test_get_api_key_for_valis_user(email=valid_email, password=valid_password):
+    """Проверяем что запрос api ключа возвращает статус 200 и в результате содержится слово key"""
+
+    # Отправляем запрос и сохраняем полученный ответ с кодом статуса в status, а текст ответа в result
     status, result = pf.get_api_key(email, password)
+
+    # Сверяем полученные данные с нашими ожиданиями
     assert status == 200
     assert "key" in result
 
 
 def test_get_all_pets_with_valid_key(filter=''):
+    """Проверяем что запрос всех питомцев возвращает не пустой список.
+    Для этого сначала получаем api ключ и сохраняем в переменную auth_key. Далее используя этого ключ
+    запрашиваем список всех питомцев и проверяем что список не пустой.
+    Доступное значение параметра filter - 'my_pets' либо '' """
+
     _, auth_key = pf.get_api_key(valid_email, valid_password)
     status, result = pf.get_list_of_pets(auth_key, filter)
+
     assert status == 200
     assert len(result['pets']) > 0
