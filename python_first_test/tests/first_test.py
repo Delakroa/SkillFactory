b32fac1581deed5e71e67a7fe8db87aedd42a0fd
Delakroa <katon1986@gmail.com>
2022-04-10 15:40:09 +0300

    дописал тесты

--- python_first_test/tests/first_test.py
+++ python_first_test/tests/first_test.py
@@ -9,5 +9,12 @@ class TestCalc:
     def test_multiply_calculate_correctly(self):
         assert self.calc.multiply(self, 2, 2) == 4
 
-    def test_multiply_calculation_failed(self):
-        assert self.calc.multiply(self, 2, 2) == 5
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, x=6, y=3) == 2
+
+    def test_subtraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, x=10, y=5) == 5
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, x=3, y=3) == 6
+
