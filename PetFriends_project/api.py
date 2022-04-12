c212627203f2aea68d639487c3d3bbb280e2a34e
Delakroa <katon1986@gmail.com>
2022-04-12 10:41:39 +0300

    new_pet_without_photo

--- PetFriends_project/api.py
+++ PetFriends_project/api.py
@@ -107,6 +107,32 @@ class PetFriends:
             result = res.text
         return status, result
 
+    def new_pet_without_photo(self, auth_key: json, name: str, animal_type: str, age: int) -> json:
+        """Метод отправляет на сервер данные о добавляемом питомце без фото и возвращает статус
+        запроса на сервер и результат в формате JSON с данными добавленного питомца"""
+
+        data = MultipartEncoder(
+            fields={
+                'name': name,
+                'animal_type': animal_type,
+                'age': age
+            }
+        )
+
+        headers = {
+            'auth_key': auth_key['key'],
+            'Content-Type': data.content_type
+        }
+
+        res = requests.post(self.base_url + 'api/create_pet_simple', headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        print(result)
+        return status, result
 
 # Первая часть практического задания 19.7.2
 
