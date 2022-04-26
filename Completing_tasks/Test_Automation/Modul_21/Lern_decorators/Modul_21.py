558140274adc5c905351d5ca2c8abb8b20d155fd
Delakroa <katon1986@gmail.com>
2022-04-26 17:43:02 +0300

    Фикстура request

--- Completing_tasks/Test_Automation/Modul_21/Lern_decorators/Modul_21.py
+++ Completing_tasks/Test_Automation/Modul_21/Lern_decorators/Modul_21.py
@@ -74,25 +74,51 @@ from config import email, password
 
 # Области видимости фикстур.
 
-@pytest.fixture(scope="class", autouse=True)
-def session_fixture():
-    print("\nclass fixture starts")
+# @pytest.fixture(scope="class", autouse=True)
+# def session_fixture():
+#     print("\nclass fixture starts")
+#
+#
+# @pytest.fixture(scope="function", autouse=True)
+# def function_fixture():
+#     print("\nfunction fixture starts")
+#
+#
+# class TestClass23:
+#
+#     def test_first(self):
+#         pass
+#
+#     def test_second(self):
+#         pass
 
-
-@pytest.fixture(scope="function", autouse=True)
-def function_fixture():
-    print("\nfunction fixture starts")
-
-
-class TestClass23:
-
-    def test_first(self):
-        pass
-
-    def test_second(self):
-        pass
 
 # ------------------------------------------------------------------------------------
 
 # Фикстура request
+
+@pytest.fixture()
+def request_fixture(request):
+    print(request.fixturename)
+    print(request.scope)
+    print(request.function.__name__)
+    print(request.cls)
+    print(request.module.__name__)
+    print(request.fspath)
+    if request.cls:
+        return f"\n У теста {request.function.__name__} класс есть\n"
+    else:
+        return f"\n У теста {request.function.__name__} класса нет\n"
+
+
+def test_request_1(request_fixture):
+    print(request_fixture)
+
+
+class TestClassRequest:
+
+    def test_request_2(self, request_fixture):
+        print(request_fixture)
+
+# ------------------------------------------------------------------------------------
 
