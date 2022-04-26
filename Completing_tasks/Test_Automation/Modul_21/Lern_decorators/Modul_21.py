04d4a9c186f47edcf0d64aac243e19969010b287
Delakroa <katon1986@gmail.com>
2022-04-26 18:47:04 +0300

    Разбор на практике

--- Completing_tasks/Test_Automation/Modul_21/Lern_decorators/Modul_21.py
+++ Completing_tasks/Test_Automation/Modul_21/Lern_decorators/Modul_21.py
@@ -97,28 +97,72 @@ from config import email, password
 
 # Фикстура request
 
-@pytest.fixture()
-def request_fixture(request):
-    print(request.fixturename)
-    print(request.scope)
-    print(request.function.__name__)
-    print(request.cls)
-    print(request.module.__name__)
-    print(request.fspath)
-    if request.cls:
-        return f"\n У теста {request.function.__name__} класс есть\n"
-    else:
-        return f"\n У теста {request.function.__name__} класса нет\n"
-
-
-def test_request_1(request_fixture):
-    print(request_fixture)
-
-
-class TestClassRequest:
-
-    def test_request_2(self, request_fixture):
-        print(request_fixture)
+# @pytest.fixture()
+# def request_fixture(request):
+#     print(request.fixturename)
+#     print(request.scope)
+#     print(request.function.__name__)
+#     print(request.cls)
+#     print(request.module.__name__)
+#     print(request.fspath)
+#     if request.cls:
+#         return f"\n У теста {request.function.__name__} класс есть\n"
+#     else:
+#         return f"\n У теста {request.function.__name__} класса нет\n"
+#
+#
+# def test_request_1(request_fixture):
+#     print(request_fixture)
+#
+#
+# class TestClassRequest:
+#
+#     def test_request_2(self, request_fixture):
+#         print(request_fixture)
 
 # ------------------------------------------------------------------------------------
+
+# Разберём на практике
+
+# Попробуем применить на практике указание области видимости фикстур и фикстуру request.
+# Как мы уже раньше заметили, нам приходится регулярно перед тестами получать id сессии, для того чтобы отправить
+# запросы в Дом Питомца.
+# Попробуем фикстуре, в которой мы получаем токен указать более глобальный scope.
+
+# @pytest.fixture(scope="class")
+# def get_key(request):
+#     # переменные email и password нужно заменить своими учетными данными
+#     response = requests.post(url='https://petfriends1.herokuapp.com/login',
+#                              data={"email": email, "pass": password})
+#     assert response.status_code == 200, 'Запрос выполнен неуспешно'
+#     assert 'Cookie' in response.request.headers, 'В запросе не передан ключ авторизации'
+#     print("\nreturn auth_key")
+#     return response.request.headers.get('Cookie')
+#
+#
+# @pytest.fixture(autouse=True)
+# def request_fixture(request):
+#     if 'Pets' in request.function.__name__:
+#         print(f"\nЗапущен тест из сьюта Дом Питомца: {request.function.__name__}")
+#
+#
+# class TestClassPets:
+#
+#     def test_getAllPets2(self, get_key):
+#         response = requests.get(url='https://petfriends1.herokuapp.com/api/pets',
+#                                 headers={"Cookie": get_key})
+#         assert response.status_code == 200, 'Запрос выполнен неуспешно'
+#         assert len(response.json().get('pets')) > 0, 'Количество питомцев не соответствует ожиданиям'
+#
+#     def test_getMyPets2(self, get_key):
+#         response = requests.get(url='https://petfriends1.herokuapp.com/my_pets',
+#                                 headers={"Cookie": get_key})
+#         assert response.status_code == 200, 'Запрос выполнен неуспешно'
+#         assert response.headers.get('Content-Type') == 'text/html; charset=utf-8'
+#
+#     def test_anotherTest(self):
+#         pass
+
+# -----------------------------------------------------------------------------------------------------------------
+
 
