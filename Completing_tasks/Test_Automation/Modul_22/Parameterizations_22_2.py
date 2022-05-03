44b490ccced8a771b5923cc9a151e05f1a6ac5ed
Delakroa <katon1986@gmail.com>
2022-05-03 15:13:45 +0300

    переписал

--- Completing_tasks/Test_Automation/Modul_22/Parameterizations_22_2.py
+++ Completing_tasks/Test_Automation/Modul_22/Parameterizations_22_2.py
@@ -23,6 +23,23 @@ from SkillFactory_practice.Completing_tasks.Test_Automation.Modul_19.Task_19_7_2
 pf = PetFriends()
 
 
+def generate_string(n):
+    return "x" * n
+
+
+def russian_chars():
+    return 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+
+def chinese_chars():
+    return '的一是不了人我在有他这为之大来以个中上们'
+
+
+# Здесь мы взяли 20 популярных китайских иероглифов
+def special_chars():
+    return '|\\/!@#$%^&*()-_=+`~?"№;:[]{}'
+
+
 @pytest.fixture(autouse=True)
 def ket_api_key():
     """ Проверяем, что запрос api-ключа возвращает статус 200 и в результате содержится слово key"""
@@ -40,8 +57,30 @@ def ket_api_key():
     assert pytest.status == 200
 
 
-@pytest.mark.parametrize("filter", ['', 'my_pets'], ids=['empty string', 'only my pets'])
-def test_get_all_pets_with_valid_key(filter):
+@pytest.mark.parametrize("filter",
+                         ['',
+                          'my_pets',
+                          generate_string(255),
+                          generate_string(1001),
+                          russian_chars(),
+                          russian_chars().upper(),
+                          chinese_chars(),
+                          special_chars(),
+                          123
+                          ],
+                         ids=
+                         [
+                             'empty string',
+                             'only my pets',
+                             '255 symbols',
+                             'more than 1000 symbols',
+                             'russian',
+                             'RUSSIAN',
+                             'chinese',
+                             'specials',
+                             'digit'
+                         ])
+def test_get_all_pets_with_negative_filter(filter):
     """ Проверяем, что запрос всех питомцев возвращает не пустой список.
     Для этого сначала получаем api-ключ и сохраняем в переменную auth_key. Далее, используя этот ключ,
     запрашиваем список всех питомцев и проверяем, что список не пустой.
@@ -49,4 +88,16 @@ def test_get_all_pets_with_valid_key(filter):
 
     pytest.status, result = pf.get_list_of_pets(pytest.key, filter)
 
+    # Проверяем статус ответа
+    assert pytest.status == 400
+
+
+@pytest.mark.parametrize("filter",
+                         ['', 'my_pets'],
+                         ids=['empti string', 'only my pets'])
+def test_get_all_pets_with_valid_key(filter):
+    pytest.status, result = pf.get_list_of_pets(pytest.key, filter)
+
+    # Проверяем статус ответа
+    assert pytest.status == 200
     assert len(result['pets']) > 0
