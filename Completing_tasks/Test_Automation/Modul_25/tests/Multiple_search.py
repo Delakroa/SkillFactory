c2808fa9897c1778fa6c66168ec6da5ad02e15e8
Delakroa <katon1986@gmail.com>
2022-06-12 11:25:33 +0300

    Отчасти рабочий вариант

--- Completing_tasks/Test_Automation/Modul_25/tests/Multiple_search.py
+++ Completing_tasks/Test_Automation/Modul_25/tests/Multiple_search.py
@@ -1,43 +1,47 @@
 #   pytest -v --driver Chrome --driver-path E:\python_libr
 # ary\SkillFaktory\SkillFactory_practice\Completing_tasks\Test_Automation\Modul_25\tests//chromedriver.exe
+
 import pytest
-from selenium import webdriver  # подключение библиотеки
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-driver_path = 'E:/python_library/chromedriver.exe'
-driver = webdriver.Chrome(executable_path=driver_path)
+DRIVER_PATH = 'E:/python_library/chromedriver.exe'
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
 
-@pytest.fixture(autouse=True)
-def testing():
-    # Переходим на страницу авторизации
+# @pytest.fixture(autouse=True)
+def test_show_all_pets():
+    """Множественный поиск"""
+    # Переходим на страницу авторизации.
     driver.get('https://petfriends.skillfactory.ru/login')
 
-    yield
-
-    driver.quit()
-
-
-def test_show_all_pets():
-    # Вводим email
+    # Вводим email.
     driver.find_element_by_id('email').send_keys('skillfaktory.qap66@gmail.com')
-    # Вводим пароль
+
+    # Вводим пароль.
     driver.find_element_by_id('pass').send_keys('7370377')
-    # Нажимаем на кнопку входа в аккаунт
+
+    # Нажимаем на кнопку входа в аккаунт.
     driver.find_element_by_css_selector('button[type="submit"]').click()
+
     # Проверяем, что мы оказались на главной странице пользователя
     assert driver.find_element_by_tag_name('h1').text == "PetFriends"
 
+    # Переходим на страницу моих питомцев
+    driver.get('https://petfriends.skillfactory.ru/my_pets')
 
-def test_attributes():
-    driver.get('https://petfriends.skillfactory.ru')
-    images = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
-    names = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
-    descriptions = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
+    # Проверяем, что мы точно оказались на странице моих питомцев
+    assert driver.find_element_by_tag_name('h2').text == "delakroa"
 
-    for i in range(len(names)):
+    # Ищем наших питомцев (картинка, имя, описание)
+    images = driver.find_elements_by_css_selector('.card-deck .card-img-top')
+    names = driver.find_elements_by_css_selector('.card-deck .card-img-top')
+    descriptions = driver.find_elements_by_css_selector('.card-deck .card-img-top')
+
+    # for i in range(len(names)):  # Старый (неудачный) способ работы с массивами.
+    for i in enumerate(names):
         assert images[i].get_attribute('src') != ''
         assert names[i].text != ''
         assert descriptions[i].text != ''
@@ -46,3 +50,15 @@ def test_attributes():
         assert len(parts[0]) > 0
         assert len(parts[1]) > 0
 
+    # print(names)
+
+    driver.quit()
+
+
+def test_attributes():
+    """Пока использовать не будем"""
+    images = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
+    names = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
+    descriptions = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
+
+    pass
