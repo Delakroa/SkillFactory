2d789b4117ed71a7e4767dcaf8481868ccf82a69
Delakroa <katon1986@gmail.com>
2022-06-12 12:27:26 +0300

    PageObject

---
+++ Completing_tasks/Test_Automation/Modul_25/tests/link.py
@@ -0,0 +1,8 @@
+pet_friends_login = "https://petfriends.skillfactory.ru/login"
+pet_friends_my_pets = "https://petfriends.skillfactory.ru/my_pets"
+
+btn_click = "button[type='submit']"
+card_deck = ".card-deck .card-img-top"
+
+valid_email = "skillfaktory.qap66@gmail.com"
+valid_password = "7370377"
