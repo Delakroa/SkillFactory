596c473d707467570f98d34b0108291bb9c6ebf9
Delakroa <katon1986@gmail.com>
2022-06-12 15:44:25 +0300

    перекинул в другой файл DRIVER_PATH

--- Completing_tasks/Test_Automation/Modul_25/tests/Multiple_search.py
+++ Completing_tasks/Test_Automation/Modul_25/tests/Multiple_search.py
@@ -1,20 +1,19 @@
 #   pytest -v --driver Chrome --driver-path E:\python_libr
 # ary\SkillFaktory\SkillFactory_practice\Completing_tasks\Test_Automation\Modul_25\tests//chromedriver.exe
-
-import pytest
+# import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from link import *
 
-DRIVER_PATH = 'E:/python_library/chromedriver.exe'
-driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-
 
 # @pytest.fixture(autouse=True)
 def test_show_all_pets():
     """Множественный поиск"""
+    driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+    # Неявное ожидание.
+    driver.implicitly_wait(10)
     # Переходим на страницу авторизации.
     driver.get(pet_friends_login)
 
@@ -27,16 +26,18 @@ def test_show_all_pets():
     # Нажимаем на кнопку входа в аккаунт.
     driver.find_element_by_css_selector(btn_click).click()
 
-    # Проверяем, что мы оказались на главной странице пользователя
+    # Проверяем, что мы оказались на главной странице пользователя.
     assert driver.find_element_by_tag_name('h1').text == "PetFriends"
 
-    # Переходим на страницу моих питомцев
+    # Переходим на страницу моих питомцев.
     driver.get(pet_friends_my_pets)
 
-    # Проверяем, что мы точно оказались на странице моих питомцев
+    # Проверяем, что мы точно оказались на странице моих питомцев.
     assert driver.find_element_by_tag_name('h2').text == "delakroa"
 
-    # Ищем наших питомцев (картинка, имя, описание)
+    # Ищем наших питомцев (картинка, имя, описание).
+    # Явное ожидание.
+    # images = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(card_deck))
     images = driver.find_elements_by_css_selector(card_deck)
     names = driver.find_elements_by_css_selector(card_deck)
     descriptions = driver.find_elements_by_css_selector(card_deck)
@@ -53,11 +54,5 @@ def test_show_all_pets():
 
     driver.quit()
 
-
-def test_attributes():
-    """Пока использовать не будем"""
-    images = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
-    names = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
-    descriptions = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
-
-    pass
+#     names = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
+#     descriptions = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
