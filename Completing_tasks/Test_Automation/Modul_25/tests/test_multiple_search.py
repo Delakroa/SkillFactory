59ba3f466118d8b948229bbd457c8a25910f1d5b
Delakroa <katon1986@gmail.com>
2022-06-13 21:33:25 +0300

    Явные и неявные ожидания

--- Completing_tasks/Test_Automation/Modul_25/tests/test_multiple_search.py
+++ Completing_tasks/Test_Automation/Modul_25/tests/test_multiple_search.py
@@ -1,22 +1,18 @@
 #   pytest -v --driver Chrome --driver-path E:\python_libr
 # ary\SkillFaktory\SkillFactory_practice\Completing_tasks\Test_Automation\Modul_25\tests//chromedriver.exe
-import pytest
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.support.ui import WebDriverWait
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from link import *
 
 
-@pytest.fixture(autouse=True)
 def test_show_all_pets(selenium):
     """Множественный поиск"""
-    # driver = webdriver.Chrome(executable_path=DRIVER_PATH)
     # Неявное ожидание.
     selenium.implicitly_wait(10)
     # Переходим на страницу авторизации.
     selenium.get(pet_friends_login)
-
     # Вводим email.
     selenium.find_element_by_id('email').send_keys(valid_email)
 
@@ -29,21 +25,14 @@ def test_show_all_pets(selenium):
     # Проверяем, что мы оказались на главной странице пользователя.
     assert selenium.find_element_by_tag_name('h1').text == "PetFriends"
 
-    # Переходим на страницу моих питомцев.
-    selenium.get(pet_friends_my_pets)
-
-    # Проверяем, что мы точно оказались на странице моих питомцев.
-    assert selenium.find_element_by_tag_name('h2').text == "delakroa"
-
     # Ищем наших питомцев (картинка, имя, описание).
     # Явное ожидание.
-    # images = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(card_deck))
-    images = selenium.find_elements_by_css_selector(card_deck)
-    names = selenium.find_elements_by_css_selector(card_deck)
-    descriptions = selenium.find_elements_by_css_selector(card_deck)
+    images = WebDriverWait(selenium, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
+    names = WebDriverWait(selenium, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
+    descriptions = WebDriverWait(selenium, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
 
-    # for i in range(len(names)):  # Старый (неудачный) способ работы с массивами.
-    for i in enumerate(names):
+    # for i in enumerate(images):
+    for i in range(len(names)):
         assert images[i].get_attribute('src') != ''
         assert names[i].text != ''
         assert descriptions[i].text != ''
@@ -54,5 +43,9 @@ def test_show_all_pets(selenium):
 
     selenium.quit()
 
-#     names = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
-#     descriptions = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
+# def pets_authorization(selenium):
+# Переходим на страницу моих питомцев.
+# selenium.get(pet_friends_my_pets)
+
+# Проверяем, что мы точно оказались на странице моих питомцев.
+# assert selenium.find_element_by_tag_name('h2').text == "delakroa"
