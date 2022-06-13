6fa1e6b26c36a2c7cb504661d434c81bb51a7d9b
Delakroa <katon1986@gmail.com>
2022-06-13 21:34:03 +0300

    card_deck_image, card_deck_names, card_deck_descriptions

--- Completing_tasks/Test_Automation/Modul_25/tests/link.py
+++ Completing_tasks/Test_Automation/Modul_25/tests/link.py
@@ -1,10 +1,13 @@
-DRIVER_PATH = 'E:/python_library/chromedriver.exe'
+# DRIVER_PATH = 'E:/python_library/chromedriver.exe'
 
 pet_friends_login = "https://petfriends.skillfactory.ru/login"
 pet_friends_my_pets = "https://petfriends.skillfactory.ru/my_pets"
 
 btn_click = "button[type='submit']"
-card_deck = ".card-deck .card-img-top"
+
+card_deck_image = ".card-deck .card-img-top"  # div[class='card-deck'] > div > div > img"
+card_deck_names = "h5[class='card-title']"
+card_deck_descriptions = "p[class='card-text']"
 
 valid_email = "skillfaktory.qap66@gmail.com"
 valid_password = "7370377"
