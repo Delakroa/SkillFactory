fc8767b66ad0b9d106315dcf1a46ac50c216504d
Delakroa <katon1986@gmail.com>
2022-06-13 21:34:18 +0300

    Пока закоментировал

--- Completing_tasks/Test_Automation/Modul_25/tests/conftest.py
+++ Completing_tasks/Test_Automation/Modul_25/tests/conftest.py
@@ -1,7 +1,7 @@
-import pytest
-
-
-@pytest.fixture
-def chrome_options(chrome_options):
-    chrome_options.set_headless(True)
-    return chrome_options
+# import pytest
+#
+#
+# @pytest.fixture
+# def chrome_options(chrome_options):
+#     chrome_options.set_headless(True)
+#     return chrome_options
