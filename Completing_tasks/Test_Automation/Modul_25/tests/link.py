d02ceb6c9ab4f9913184a1a8b056a3932eea9219
Delakroa <katon1986@gmail.com>
2022-06-15 10:45:31 +0300

    Добавил данные

--- Completing_tasks/Test_Automation/Modul_25/tests/link.py
+++ Completing_tasks/Test_Automation/Modul_25/tests/link.py
@@ -1,13 +1,17 @@
-# DRIVER_PATH = 'E:/python_library/chromedriver.exe'
+DRIVER_PATH = 'E:/python_library/chromedriver.exe'
 
-pet_friends_login = "https://petfriends.skillfactory.ru/login"
-pet_friends_my_pets = "https://petfriends.skillfactory.ru/my_pets"
+pet_friends_login = 'https://petfriends.skillfactory.ru/login'
+pet_friends_my_pets = 'https://petfriends.skillfactory.ru/my_pets'
 
-btn_click = "button[type='submit']"
+btn_click = 'button[type="submit"]'
 
-card_deck_image = ".card-deck .card-img-top"  # div[class='card-deck'] > div > div > img"
-card_deck_names = "h5[class='card-title']"
-card_deck_descriptions = "p[class='card-text']"
+all_card_deck_image = '.card-deck .card-img-top'
+all_card_deck_names = '.card-deck .card-title'
+all_card_deck_descriptions = '.card-deck .card-text'
 
-valid_email = "skillfaktory.qap66@gmail.com"
-valid_password = "7370377"
+# my_pets_css_selector =
+# my_pets
+# my_pets
+
+valid_email = 'skillfaktory.qap66@gmail.com'
+valid_password = '7370377'
