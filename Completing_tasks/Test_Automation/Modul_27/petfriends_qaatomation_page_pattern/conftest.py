7b57b31710d61c0e3038bcdaab7aa0486492f994
Delakroa <katon1986@gmail.com>
2022-06-19 14:14:42 +0300

    Перевод коментариев

--- Completing_tasks/Test_Automation/Modul_27/petfriends_qaatomation_page_pattern/conftest.py
+++ Completing_tasks/Test_Automation/Modul_27/petfriends_qaatomation_page_pattern/conftest.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- encoding=utf8 -*-
 
-# This is example shows how we can manage failed tests
-# and make screenshots after any failed test case.
+# Это пример показывает, как мы можем управлять неудачными тестами
+# и сделайте скриншоты после любого неудачного тестового примера.
 
 import pytest
 import uuid
@@ -21,9 +21,8 @@ def chrome_options(chrome_options):
 
 @pytest.hookimpl(hookwrapper=True, tryfirst=True)
 def pytest_runtest_makereport(item, call):
-    # This function helps to detect that some test failed
-    # and pass this information to teardown:
-
+    # Эта функция помогает обнаружить, что какой -то тест вышел из строя
+    # и передайте эту информацию Tapdown:
     outcome = yield
     rep = outcome.get_result()
     setattr(item, "rep_" + rep.when, rep)
@@ -36,54 +35,54 @@ def web_browser(request, selenium):
     browser = selenium
     browser.set_window_size(1400, 1000)
 
-    # Return browser instance to test case:
+    # Вернуть экземпляр браузера для тестирования примера:
     yield browser
 
-    # Do teardown (this code will be executed after each test):
+    # Do Teardown (этот код будет выполнен после каждого теста):
 
     if request.node.rep_call.failed:
-        # Make the screen-shot if test failed:
+        # Сделайте съемки экрана, если тест не удался:
         try:
             browser.execute_script("document.body.bgColor = 'white';")
 
-            # Make screen-shot for local debug:
+            # Сделайте Screen Shot для локальной отладки:
             browser.save_screenshot('screenshots/' + str(uuid.uuid4()) + '.png')
 
-            # Attach screenshot to Allure report:
+            # Прикрепите скриншот к отчету Allure:
             allure.attach(browser.get_screenshot_as_png(),
                           name=request.function.__name__,
                           attachment_type=allure.attachment_type.PNG)
 
-            # For happy debugging:
+            # Для счастливой отладки:
             print('URL: ', browser.current_url)
             print('Browser logs:')
             for log in browser.get_log('browser'):
                 print(log)
 
         except:
-            pass # just ignore any errors here
+            pass # просто игнорируйте здесь любые ошибки
 
 
 def get_test_case_docstring(item):
-    """ This function gets doc string from test case and format it
-        to show this docstring instead of the test case name in reports.
+    """ Эта функция получает строку DOC от тестового примера и отформатирует ее
+        Чтобы показать этот Docstring вместо названия тестового примера в отчетах.
     """
 
     full_name = ''
 
     if item._obj.__doc__:
-        # Remove extra whitespaces from the doc string:
+        # Удалить дополнительные пробелы из строки DOC:
         name = str(item._obj.__doc__.split('.')[0]).strip()
         full_name = ' '.join(name.split())
 
-        # Generate the list of parameters for parametrized test cases:
+        # Сгенерировать список параметров для параметризованных тестовых случаев:
         if hasattr(item, 'callspec'):
             params = item.callspec.params
 
             res_keys = sorted([k for k in params])
-            # Create List based on Dict:
+            # Создать список на основе DICT:
             res = ['{0}_"{1}"'.format(k, params[k]) for k in res_keys]
-            # Add dict with all parameters to the name of test case:
+            # Добавить дикта со всеми параметрами к названию тестового примера:
             full_name += ' Parameters ' + str(', '.join(res))
             full_name = full_name.replace(':', '')
 
@@ -91,8 +90,8 @@ def get_test_case_docstring(item):
 
 
 def pytest_itemcollected(item):
-    """ This function modifies names of test cases "on the fly"
-        during the execution of test cases.
+    """ Эта функция изменяет имена тестовых случаев "на лету"
+        Во время выполнения тестовых случаев.
     """
 
     if item._obj.__doc__:
@@ -100,16 +99,16 @@ def pytest_itemcollected(item):
 
 
 def pytest_collection_finish(session):
-    """ This function modified names of test cases "on the fly"
-        when we are using --collect-only parameter for pytest
-        (to get the full list of all existing test cases).
+    """ Эта функция изменила имена тестовых случаев "на лету"
+        Когда мы используем параметр только для Pytest только для Pytest
+        (Чтобы получить полный список всех существующих тестовых случаев).
     """
 
     if session.config.option.collectonly is True:
         for item in session.items:
-            # If test case has a doc string we need to modify it's name to
-            # it's doc string to show human-readable reports and to
-            # automatically import test cases to test management system.
+            # Если в тестировании есть строка DOC, нам нужно изменить его имя на
+            # это Doc String, чтобы показать читаемые человеческие отчеты и
+            # автоматически импортируйте тестовые примеры в систему управления тестированием.
             if item._obj.__doc__:
                 full_name = get_test_case_docstring(item)
                 print(full_name)
