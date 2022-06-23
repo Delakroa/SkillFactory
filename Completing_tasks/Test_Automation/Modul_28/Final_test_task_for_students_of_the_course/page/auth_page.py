a43130fef2bb353b636ccaa1128307a42119c86c
Delakroa <katon1986@gmail.com>
2022-06-23 20:03:11 +0300

    Создание base & auth_page

--- Completing_tasks/Test_Automation/Modul_28/Final_test_task_for_students_of_the_course/page/auth_page.py
+++ Completing_tasks/Test_Automation/Modul_28/Final_test_task_for_students_of_the_course/page/auth_page.py
@@ -1,2 +1,15 @@
-#!/usr/bin/python3
-# -*- encoding=utf8 -*-
+from base import WebPage
+from elements import WebElement
+
+
+class AuthPage(WebPage):
+
+    def __init__(self, web_driver, url=''):
+        url = 'https://www.ozon.ru/'
+        super().__init__(web_driver, url)
+
+    email = WebElement(id='email')    # skillfaktory.qap66@gmail.com
+
+    password = WebElement(id='pass')  # 7370377
+
+    btn = WebElement(class_name='btn.btn-success')
