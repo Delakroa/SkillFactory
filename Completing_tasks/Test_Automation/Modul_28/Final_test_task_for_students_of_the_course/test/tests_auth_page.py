89c84ff62310a7ba6255a8966ac15e829a4ca76b
Delakroa <katon1986@gmail.com>
2022-06-26 13:24:02 +0300

    https://www.labirint.ru/

--- Completing_tasks/Test_Automation/Modul_28/Final_test_task_for_students_of_the_course/test/tests_auth_page.py
+++ Completing_tasks/Test_Automation/Modul_28/Final_test_task_for_students_of_the_course/test/tests_auth_page.py
@@ -1,2 +1,14 @@
-#!/usr/bin/python3
-# -*- encoding=utf8 -*-
+import pytest
+from pages.auth_page import AuthPage
+
+
+def test_authorisation(web_browser):
+    page = AuthPage(web_browser)
+
+    page.email.send_keys('test123@yandex.ru')
+
+    page.password.send_keys("123456")
+
+    page.btn.click()
+
+    assert page.get_current_url() == ''
