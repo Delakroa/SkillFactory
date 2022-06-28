50a341c1af95bb1d568d7a00e851e6168914d723
Delakroa <katon1986@gmail.com>
2022-06-28 19:38:06 +0300

    Тест страницы авторизации

--- Completing_tasks/Test_Automation/Modul_28/Final_test_task_for_students_of_the_course/test/tests_auth_page.py
+++ Completing_tasks/Test_Automation/Modul_28/Final_test_task_for_students_of_the_course/test/tests_auth_page.py
@@ -1,3 +1,5 @@
+# python -m pytest -v --driver Chrome --driver-path
+# E:\python_library\SkillFaktory\SkillFactory_practice\Completing_tasks\Test_Automation\Modul_28\Final_test_task_for_students_of_the_course\chromedriver.exe
 import pytest
 from pages.auth_page import AuthPage
 
@@ -5,10 +7,12 @@ from pages.auth_page import AuthPage
 def test_authorisation(web_browser):
     page = AuthPage(web_browser)
 
-    page.email.send_keys('test123@yandex.ru')
-
-    page.password.send_keys("123456")
+    page.email.send_keys('skillfaktory.qap66@gmail.com')
 
     page.btn.click()
 
-    assert page.get_current_url() == ''
+    page.password.send_keys("2600-421A-95EF")
+
+    page.btn.click()
+
+    assert page.get_current_url() == 'https://www.labirint.ru/cabinet/'
