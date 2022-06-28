1257fd8bf46d46464072eb5b2880f82eefc7b5bf
Delakroa <katon1986@gmail.com>
2022-06-28 19:37:16 +0300

    добавил локаторы

--- Completing_tasks/Test_Automation/Modul_28/Final_test_task_for_students_of_the_course/page/auth_page.py
+++ Completing_tasks/Test_Automation/Modul_28/Final_test_task_for_students_of_the_course/page/auth_page.py
@@ -5,11 +5,13 @@ from elements import WebElement
 class AuthPage(WebPage):
 
     def __init__(self, web_driver, url=''):
-        url = 'https://www.labirint.ru/'
+        url = 'https://www.labirint.ru/cabinet/'
         super().__init__(web_driver, url)
 
-    email = WebElement(id='email')    # skillfaktory.qap66@gmail.com
+    email = WebElement(id='_inputnamecode_59')  # skillfaktory.qap66@gmail.com
 
-    password = WebElement(id='pass')  # 7370377
+    btn_1 = WebElement(id='g-recap-0-btn')
 
-    btn = WebElement(class_name='btn.btn-success')
+    password = WebElement(id='_inputnamecode_28')  # 7370377
+
+    btn_2 = WebElement(class_name='new-auth__button.js-submitnew-auth__input.full-input__input.new-forms__input_size_m')
